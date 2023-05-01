7280c243d53ed4b37ad5b52e1a7534b1c2e091c4
liriumgames <yosefedery@hotmail.es>
2023-05-01 11:29:22 +0300

    de

--- MACROS.h
+++ MACROS.h
@@ -12,6 +12,7 @@
 #define FIRST_MESSAGE_SIZE 1
 #define INCREMENT_MESSAGE_FACTOR 2
 #define MB_1 1024 * 1024
+// We've chose 1000 messages since it takes around
 #define K_NUM_MESSAGES 1000.L
 #define DEFAULT_PORT 8081
 
